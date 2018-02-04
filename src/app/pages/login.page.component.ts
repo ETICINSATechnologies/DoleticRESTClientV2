c151d28f7308d6be244e23d8a089223621a0ad9c
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2018-02-04 18:16:03 +0100

    Correction du push précédent

--- src/app/pages/login.page.component.ts
+++ src/app/pages/login.page.component.ts
@@ -2,23 +2,21 @@ import {Component} from '@angular/core';
 import {LoginService} from '../services/login.service';
 import {LoginID} from '../entities/loginID';
 import {Router} from "@angular/router";
-import {SessionService} from "../services/session.service";
 
 @Component({
     selector: 'doletic-login',
     templateUrl: '../html/login.page.component.html',
-    providers: [LoginService, SessionService]
+    providers: [LoginService]
 })
 
 export class LoginPageComponent {
     identifiants: LoginID = new LoginID('', '');
-    constructor(private loginService: LoginService,private sessionService: SessionService, private router: Router) {}
+    constructor(private loginService: LoginService, private router: Router) {}
     login() {
       this.loginService.login(this.identifiants)
       this.loginService.login(this.identifiants)
         .then(res => {
           this.router.navigate(['/dashboard']);
-          window.sessionStorage.setItem('logged', "true");
         })
         .catch();
     }
