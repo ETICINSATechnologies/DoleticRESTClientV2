beee0352f629b4e2d542a6c435f6c825f71d2e6d
Unknown <azergante@gmail.com>
2018-02-11 21:18:29 +0100

    Le composant CompanyTable hérite maintenant de table-component.ts
    Mise en place de l'héritage pour les composants tableaux
    
    Ajout d'un modèle html pour les template tableaux (pour l'instant il sert juste de référence et n'est jamais appelé par le code)
    
    La méthode qui transforme l'objet en string[][] est placée dans le service (ce sont toujours des données de test)
    
    Bouton pour réinitialiser les filtres

---
+++ src/app/entities/table-template.ts
@@ -0,0 +1,98 @@
+import { OnInit } from '@angular/core';
+
+export class TableTemplate implements OnInit {
+	PAGE_LENGTHS: number[] = [5, 10, 25, 50];
+  page_length: number = 5;
+
+  page_no: number = 1;
+
+  sorted_column: number = -1;
+  sort_order: number = 0; /*1 : lower comes first (ascending sort) 
+                          -1 : greater comes first (descending sort)*/ 
+
+  headers: string[]; // overridden in subclass
+	data: string[][]; // headers + id
+
+  filtered_data: string[][] = [];
+  filter_criteria: string[] = [];
+
+  constructor(private service: any) { } // overridden in subclass 
+
+  ngOnInit()
+  {
+  	/*this.service.getTableData()
+      .then(remoteData => 
+      {
+        this.data = remoteData;
+        this.refreshData();
+      }); */
+      this.data = this.service.getTableData();
+      this.refreshData();
+  }
+
+  refreshData(): void
+  {
+    if(this.sorted_column >= 0) 
+    { // sorts the table if needed when data is re-fetched
+      this.sortData(this.sorted_column);
+    } 
+    else
+    { // refreshes displayed data
+      this.filterData();
+    } 
+  }
+
+  filterData(): void
+  {
+  	let search_buffer: string[][] = this.data;
+    for (let i = 0; i < this.filter_criteria.length; i++) 
+    {
+      const term:string = this.filter_criteria[i];
+      if(term)
+      {
+        const regex: RegExp = new RegExp(term.trim().replace(/\s+/g,'|'), 'iu');
+        search_buffer = search_buffer.filter(tuple => regex.test(tuple[i]));
+      }    
+    }
+	  this.filtered_data = search_buffer;
+  }
+
+  sortData(column: number): void
+  {
+    if(column==this.sorted_column)
+    {
+      this.sort_order*=-1;
+    }
+    else
+    {
+      this.sorted_column = column;
+      this.sort_order = 1;
+    }
+    this.data.sort( (left, right) => 
+      {
+        if(left[column] < right[column]) return -this.sort_order; // left comes first
+        else if (left[column] > right[column]) return this.sort_order;
+        else return 0;
+      }
+    );
+    this.filterData(); // reflects the sort on filtered data
+  } 
+
+  resetFilters(): void
+  {
+    this.filter_criteria = [];
+    this.filtered_data = this.data;
+  }
+  
+  // suppose que le service place l'ID dans la dernière colonne de data
+  getId(tuple:string[]): string
+  { 
+    return tuple[tuple.length-1];
+  }
+  ceil(x: number): number {
+    return Math.ceil(x);
+  }
+  min(x: number, y: number): number {
+    return Math.min(x, y);
+  }
+}
