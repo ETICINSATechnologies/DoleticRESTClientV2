f45f67d639f1bb88abbf1e632ae37758b61d57bf
Conor Ryan <cr.in.france@gmail.com>
2018-02-19 20:14:07 +0100

    Début Tableau GRC3

--- src/app/components/GRC/company-table.component.ts
+++ src/app/components/GRC/company-table.component.ts
@@ -1,22 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { TableTemplate } from '../../entities/table-template'
 
 import { FirmService } from '../../services/firm.service'
+import { Firm } from '../../entities/firm';
+import { FirmTypeService } from '../../services/firm-type.service';
+import { FirmType } from '../../entities/firm-type';
 
 @Component({
   selector: 'doletic-company-table',
   templateUrl: '../../html/company-table.component.html',
-  providers: [FirmService]
+  providers: [FirmService, FirmTypeService]
 })
-export class CompanyTableComponent extends TableTemplate {
+export class CompanyTableComponent extends TableTemplate implements OnInit {
 
   headers: string[] = ["Nom", "SIRET", "Type", "Adresse", "Code Postal", "Ville", "Pays"]; // + id
+  activeFirm: Firm;
 
-  constructor(service: FirmService) 
+  showEditFirm: boolean = false;
+  errorEditFirm: boolean = false;
+  loadingEditFirm: boolean = false;
+
+  firmTypes: FirmType[];
+
+  constructor(
+    service: FirmService,
+    private firmTypeService: FirmTypeService)
   {
-    super(service);
+    super(service)
   }
 
- 
+  ngOnInit(): void {
+    super.ngOnInit();
+    this.loadFirmTypes();
+  }
+
+  startEditFirm(id : number) {
+    this.service.getById(id).then(firm => {
+      this.activeFirm = firm;
+      this.showEditFirm = true;
+    }).catch(res => console.log('Error in startEditFirm : ' + res));
+  }
+
+  cancelEditFirm(): void {
+    this.showEditFirm = false;
+    this.errorEditFirm = false;
+    this.loadingEditFirm = false;
+  }
+
+  loadFirmTypes(): void {
+    this.firmTypeService.getAll().then(
+      res => {
+        this.firmTypes = <FirmType[]>res;
+      }
+    ).catch( res => {console.log('Error in getFirmTypes : ' + res);})
+  }
+
 }
