1089b285bd95d9d57bfb1ef577a5defc4a2263ce
Justin GALLY <justin.gallysp@gmail.com>
2018-03-15 13:32:09 +0100

    Update company-table.component.ts

--- src/app/components/GRC/company-table.component.ts
+++ src/app/components/GRC/company-table.component.ts
@@ -16,16 +16,5 @@ export class CompanyTableComponent extends TableTemplate {
   constructor(private firmService: FirmService) {
     super(firmService)
   }
-
-  loadData(remoteData: any): void {
-    for (let i = remoteData.length - 1; i >= 0; i--) {
-      this.data.push(
-        [
-          remoteData[i].name, remoteData[i].siret, remoteData[i].type.label,
-          remoteData[i].adress, remoteData[i].postalCode, remoteData[i].city,
-          remoteData[i].country.label, remoteData[i].id
-        ]);
-    }
-  }
 }
 
